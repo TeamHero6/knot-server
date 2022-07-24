107a7302c53371290b2e7a412f956aff295b169f
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-24 08:11:24 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//middleWare
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@knot.c1izps4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        client.connect();
+        console.log("connected");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Knot server running on port ${port}`);
+});
